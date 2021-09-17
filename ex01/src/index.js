e19e4df5ed1a729e046716311de0e3c0cec5b68f
Amir Muharemovic <muharemovic.amir@gmail.com>
2021-09-17 17:50:21 +0200

    Dodao zadatke za peti dan

---
+++ ex01/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    // Only change code below this line
+    myFirstString = "I think therefore I \"am\" -- sentient.";
+    mySecondString = "No, you only \"think\", so you aren\'t."; 
+    // Only change code above this line
+    return { myFirstString,
+         mySecondString };
+  }
+  
+  console.log(main());
+  module.exports = main;
+  
